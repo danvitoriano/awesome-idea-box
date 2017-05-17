af5c88c96ef13c4974c5230b78b781be45f3f34d
Danilo Vitoriano <vitoriano08@gmail.com>
2017-05-17 01:14:54 -0300

    add browseroute, add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AutorBox from './Autor';
 import LivroBox from './Livro';
@@ -8,12 +9,15 @@ import './index.css';
 import {Router,Route,browserHistory,IndexRoute} from 'react-router';
 
 ReactDOM.render(
-  (<Router history={browserHistory}>
+  (<BrowserRouter basename="/awesome-idea-box">
+	
   		<Route path="/" component={App}>
   			<IndexRoute component={Home}/>
   			<Route path="/autor" component={AutorBox}/>
   			<Route path="/livro" component={LivroBox}/>
   		</Route>
-   </Router>),
+  	</BrowserRouter>
+   
+   ),
   document.getElementById('root')
 );
