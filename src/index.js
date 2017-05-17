fb3420cb0b42beb49a95acf7638de755619e7b3a
Danilo Vitoriano <danilo.vitoriano@oi.net.br>
2017-05-17 15:56:18 -0300

    change router to hashhistory

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AutorBox from './Autor';
 import LivroBox from './Livro';
 import Home from './Home';
 import './index.css';
-import {Router,Route,browserHistory,IndexRoute} from 'react-router';
+import {Route,Router,hashHistory,IndexRoute} from 'react-router';
 
 ReactDOM.render(
-  (<BrowserRouter basename="/awesome-idea-box">
-	
-  		<Route path="/" component={App}>
-  			<IndexRoute component={Home}/>
-  			<Route path="/autor" component={AutorBox}/>
-  			<Route path="/livro" component={LivroBox}/>
-  		</Route>
-  	</BrowserRouter>
-   
+  (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="/autor" component={AutorBox}/>
+        <Route path="/livro" component={LivroBox}/>
+      </Route>
+  </Router>
    ),
   document.getElementById('root')
 );
